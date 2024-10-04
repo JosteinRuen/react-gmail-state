680dec1ad70af1b6f9f23304c8564b2a01efda7b
JosteinRuen <99617155+JosteinRuen@users.noreply.github.com>
2024-10-04 13:41:19 +0200

    State core

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Header from './components/Header'
 import initialEmails from './data/emails'
+import {useState} from 'react';
 
 import './styles/App.css'
 
 function App() {
   // Use initialEmails for state
   console.log(initialEmails)
+  const[emails, setEmails] = useState(initialEmails)
 
   return (
     <div className="app">
@@ -13,18 +15,16 @@ function App() {
       <nav className="left-menu">
         <ul className="inbox-list">
           <li
-            className="item active"
-            // onClick={() => {}}
-          >
+            className="item active">
             <span className="label">Inbox</span>
-            <span className="count">?</span>
+            <span className="count">{emails.filter(email => !email.read).length}</span>
           </li>
           <li
             className="item"
             // onClick={() => {}}
           >
             <span className="label">Starred</span>
-            <span className="count">?</span>
+            <span className="count">{emails.filter(email => email.starred).length}</span>
           </li>
 
           <li className="item toggle">
@@ -38,7 +38,30 @@ function App() {
           </li>
         </ul>
       </nav>
-      <main className="emails">{/* Render a list of emails here */}</main>
+      <main className="emails">
+        <ul className="email-list">
+          {emails.map(email => (
+            <li key={email.id} className={`email ${email.read ? 'read' : 'unread'}`}>
+              <div className="select">
+                <input
+                  className="select-checkbox"
+                  type="checkbox"
+                />
+              </div>
+              <div className="star">
+                <input
+                  className="star-checkbox"
+                  type="checkbox"
+                  checked={email.starred}
+                  // onChange={() => {}}
+                />
+              </div>
+              <div className="sender">{email.sender}</div>
+              <div className="title">{email.title}</div>
+            </li>
+          ))}
+        </ul>
+      </main>
     </div>
   )
 }
